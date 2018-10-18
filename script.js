06ae5258bdce465df30fb16b1a5ca24e5954f526
Syedkhizer <38943055+syedkhizerazmi@users.noreply.github.com>
2018-10-18 17:38:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
+
+
+ M.toast({html: 'I am a toast!'})
+
